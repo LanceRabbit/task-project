526b318465237f4ab6b23b9de342ce2430d351f4
LanceRabbit <lancerabbit@gmail.com>
2020-03-17 20:14:18 +0800

    add new and create action, and new page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,23 @@ class TasksController < ApplicationController
     end
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render new_task_path
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
 end
