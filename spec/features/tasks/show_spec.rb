1ffc52da52a51c6283b2499219579e3badfd2529
LanceRabbit <lancerabbit@gmail.com>
2020-03-22 23:36:30 +0800

    add task show feature test

---
+++ spec/features/tasks/show_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature "ShowTask", type: :feature do
+  let(:task) { FactoryBot.create(:task) }
+  scenario "show page will see the title and content" do
+    @task = task
+    visit tasks_path
+    find('input[value="Show"]').click
+    expect(page).to have_content @task.title
+    expect(page).to have_content @task.content
+  end
+end
