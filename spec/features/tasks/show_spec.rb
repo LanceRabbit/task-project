819d79ac10688958894332c2c20711a04aac7ad9
LanceRabbit <lancerabbit@gmail.com>
2020-03-26 20:27:55 +0800

    setup i18n on spec testing

--- spec/features/tasks/show_spec.rb
+++ spec/features/tasks/show_spec.rb
@@ -5,7 +5,8 @@ RSpec.feature "ShowTask", type: :feature do
   scenario "show page will see the title and content" do
     @task = task
     visit tasks_path
-    find('input[value="Show"]').click
+    # find('input[value="Show"]').click
+    click_button I18n.t('views.show')
     expect(page).to have_content @task.title
     expect(page).to have_content @task.content
   end
