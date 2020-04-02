aacad85f8dbd5ed7997fc32bf6498e90801434f1
LanceRabbit <lancerabbit@gmail.com>
2020-04-02 20:25:34 +0800

    add list task by oreder by time test code

--- spec/features/tasks/index_spec.rb
+++ spec/features/tasks/index_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  scenario "index page have one task" do
-    FactoryBot.create(:task)
-    visit tasks_path
-    expect(page).to have_content "task-1"
+  feature "Index is work" do
+    scenario "index page have one task" do
+      FactoryBot.create(:task)
+      visit tasks_path
+      expect(page).to have_content "task-1"
+    end
+
+    scenario "list task order by created time" do
+      FactoryBot.create(:task,  created_at: Date.today.weeks_ago(1))
+      task = FactoryBot.create(:task, :second_task, created_at: Date.today)
+      visit tasks_path
+      expect(
+        find('table tbody tr:nth-child(1) td:nth-child(2)')
+      ).to have_content(task.title)
+    end
   end
 end
