782381e19e2037431d2f355a57ecb413c3b0947f
LanceRabbit <lancerabbit@gmail.com>
2020-04-04 14:13:13 +0800

    add gem ransack and setup setting on applcation

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative 'boot'
-
+# https://github.com/activerecord-hackery/ransack#using-simpleform
+ENV['RANSACK_FORM_BUILDER'] = '::SimpleForm::FormBuilder'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
