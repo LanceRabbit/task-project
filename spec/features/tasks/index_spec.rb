ae5869303fa761d06b9cc19c1f367ac435f14a1f
LanceRabbit <lancerabbit@gmail.com>
2020-04-04 11:11:20 +0800

    add sorting test code on task index page

--- spec/features/tasks/index_spec.rb
+++ spec/features/tasks/index_spec.rb
@@ -2,19 +2,28 @@ require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
   feature "Index is work" do
-    scenario "index page have one task" do
-      FactoryBot.create(:task)
+    before(:each) do
+      @task_1 = FactoryBot.create(:task,  created_at: Date.today.weeks_ago(1))
+      @task_2 = FactoryBot.create(:task, :second_task, end_date: Date.today + 4)
       visit tasks_path
+    end
+
+    scenario "index page have one task" do
       expect(page).to have_content "task-1"
     end
 
-    scenario "list task order by created time" do
-      FactoryBot.create(:task,  created_at: Date.today.weeks_ago(1))
-      task = FactoryBot.create(:task, :second_task, created_at: Date.today)
-      visit tasks_path
+    scenario "list task order by created_at" do
+      click_link I18n.t('views.tasks.created_at')
       expect(
         find('table tbody tr:nth-child(1) td:nth-child(2)')
-      ).to have_content(task.title)
+      ).to have_content(@task_2.title)
+    end
+
+    scenario "list task order by end_date" do
+      click_link I18n.t('views.tasks.end_date')
+      expect(
+        find('table tbody tr:nth-child(1) td:nth-child(2)')
+      ).to have_content(@task_1.title)
     end
   end
 end
