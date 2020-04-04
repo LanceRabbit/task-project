a8a535b7ba2422215de6f5a1fa2c5e9fb58b87ab
LanceRabbit <lancerabbit@gmail.com>
2020-04-04 22:05:59 +0800

    add priority sorting test code

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     start_date { Date.today }
     end_date { Date.today + 5 }
     state { :todo }
+    priority { :low }
 
     trait :second_task do
       title { "task-2" }
@@ -16,6 +17,16 @@ FactoryBot.define do
       title { "task-3" }
       content { "task-content-3" }
       state { :completed }
+    end
+
+    trait :high_task do
+      title { "high-task" }
+      priority { :high }
+    end
+
+    trait :medium_task do
+      title { "medium-task" }
+      priority { :medium }
     end
   end
 end
