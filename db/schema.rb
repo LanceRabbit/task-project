5b9ac21a2bb575d49600ca18460e1afc683ca4e9
LanceRabbit <lancerabbit@gmail.com>
2020-04-05 15:36:10 +0800

    add user_id on task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_05_063207) do
+ActiveRecord::Schema.define(version: 2020_04_05_071300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2020_04_05_063207) do
     t.date "end_date"
     t.integer "state", default: 0, null: false
     t.integer "priority", default: 0, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
