832b35325b8ddae5bd097dbed0c4e5f0839b76c3
LanceRabbit <lancerabbit@gmail.com>
2020-04-05 16:11:03 +0800

    fix rspec error

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
+    sequence(:name) {|n| "User #{n}"}
+    sequence(:email) {|n| "user_#{n}@example.com"}
   end
 end
