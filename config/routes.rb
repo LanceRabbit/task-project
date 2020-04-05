17569d2f80a2eddc512bf3a4872cfb37955252e4
LanceRabbit <lancerabbit@gmail.com>
2020-04-05 20:33:48 +0800

    add session controller and setup login and logut functions

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,11 @@ Rails.application.routes.draw do
   end
 
   resources :users
+  get 'signup', to: 'users#new', as: 'signup'
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
 end
