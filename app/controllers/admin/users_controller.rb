e2e3f803df8c692e10c5d188eff301f61d727648
LanceRabbit <lancerabbit@gmail.com>
2020-04-06 06:18:01 +0800

    add admin users show page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,17 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: %i[edit update show destroy]
+  before_action :check_user_exist?, only: %i[edit update show destroy]
+
   def index
     @users = User.page(params[:page]).per(10)
   end
 
   def show
+    @q = Task.ransack(params[:q])
+    @tasks = @q.result
+      .order_created(@user.id, Task::SORT_OPTIONS[:created_at])
+      .page(params[:page])
+      .per(3)
   end
 
   def new
@@ -19,5 +27,18 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def check_user_exist?
+    if @user.nil?
+      flash[:alert] = t(".alert")
+      redirect_to admin_root_path
+    end
   end
 end
