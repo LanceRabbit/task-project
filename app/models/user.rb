dab09d74abf9b8fa46abbe5beaae0f945352ad50
LanceRabbit <lancerabbit@gmail.com>
2020-04-08 17:07:43 +0800

    fix rubocop check

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   before_save :email_downcase
   before_destroy :keep_one_admin
-  validates :email, presence: true, uniqueness: true,
-            format: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  validates :email, presence: true, uniqueness: true, format: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+
   validates :password, presence: true, length: { minimum: 8 }
 
   has_secure_password
   has_many :tasks, dependent: :destroy
 
-  enum role: %i[normal admin]
+  enum role: %i(normal admin)
 
   def email_downcase
     self.email = email.downcase
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   end
 
   def keep_one_admin
-    return unless self.admin?
+    return unless admin?
     if User.where(role: :admin).count == 1
       errors.add(:base, I18n.t('users.keep_one_admin'))
       throw :abort
--- spec/features/tasks/update_spec.rb
+++ spec/features/tasks/update_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "UpdateTask", type: :feature do
       @task = FactoryBot.create(:task, user_id: @user.id)
 
       visit login_path
-      fill_in 'session_email',    with: @user.email
+      fill_in 'session_email', with: @user.email
       fill_in 'session_password', with: @user.password
       click_button I18n.t('views.users.login')
     end
@@ -15,7 +15,7 @@ RSpec.feature "UpdateTask", type: :feature do
     scenario "update task at index page" do
       # find('input[value="Edit"]').click
       click_link I18n.t('views.edit')
-      fill_in 'task[title]',	with: 'update title'
+      fill_in 'task[title]', with: 'update title'
       fill_in 'task[content]', with: 'change content'
       click_button I18n.t('views.update')
       expect(page).to have_content('update title')
@@ -24,7 +24,7 @@ RSpec.feature "UpdateTask", type: :feature do
     scenario "update task at show page" do
       click_link I18n.t('views.show')
       click_link I18n.t('views.edit')
-      fill_in 'task[title]',	with: 'update title'
+      fill_in 'task[title]', with: 'update title'
       fill_in 'task[content]', with: 'change content'
       click_button I18n.t('views.update')
       expect(page).to have_content('update title')
